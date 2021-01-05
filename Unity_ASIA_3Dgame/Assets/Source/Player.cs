fad50ee8924296583b9612d28bbcc747279861dc
hgook120 <55180627+hgook120@users.noreply.github.com>
2021-01-05 16:19:23 +0800

    20200105_01

---
+++ Unity_ASIA_3Dgame/Assets/Source/Player.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using Invector.vCharacterController; //引用 套件
+
+public class Player : MonoBehaviour
+{
+    private float hp = 100;
+    private Animator ani;
+
+    private void Awake()
+    {
+        ani = GetComponent<Animator>();
+    }
+
+    /// <summary>
+    /// 受傷
+    /// </summary>
+    public void Damage()
+    {
+        //print("受傷");
+        hp -= 35;
+        ani.SetTrigger("受傷觸發");
+
+        if (hp <= 0)
+        {
+            Dead();
+        }
+    }
+
+    private void Dead()
+    {
+        ani.SetTrigger("死亡觸發");
+
+        //鎖定移動與旋轉
+        vThirdPersonController vt = GetComponent<vThirdPersonController>();
+        vt.lockMovement = true;
+        vt.lockRotation = true;
+    }
+}
